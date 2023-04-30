f9f543a7e77136bd6eb8b68b6751f1f8f253a10a
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2023-04-30 14:13:04 -0300

    Clima status enum created

---
+++ src/main/java/br/udesc/pin/metragem/metragemapi/model/enums/Clima.java
@@ -0,0 +1,29 @@
+package br.udesc.pin.metragem.metragemapi.model.enums;
+
+public enum Clima {
+    
+    BOM(1),
+    NUBLADO(2),
+    CHUVOSO(3);
+
+    private int codigo;
+
+    private Clima(int codigo){
+        this.codigo = codigo;
+    }
+
+    public int getCodigo(){
+        return this.codigo;
+    }
+
+    public static Clima valueOf(int codigo){
+        for(Clima cli : Clima.values()){
+            if(cli.getCodigo() == codigo){
+                return cli;
+            }
+        }
+        throw new IllegalArgumentException("Código do Clima inválido.");
+    }
+
+
+}
